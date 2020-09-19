fbcfa8a1f18a04946994c7190cb6f92e74a69724
Adhiraj <adhirajsingh1001@gmail.com>
2020-09-20 02:55:48 +0530

    Update corona.js

--- intents/corona.js
+++ intents/corona.js
@@ -1,5 +1,6 @@
 // from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/NumberFormat
 const formatted = n => new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(+n)
+const API_URL = "https://api.covid19india.org/data.json"
 
 module.exports = class {
     constructor (processor) {
@@ -17,7 +18,7 @@ module.exports = class {
     }
     async fetchData () {
         const fetch = require("node-fetch")
-        let API_URL = "https://api.covid19india.org/data.json"
+
         try {
             const response = await fetch (API_URL)
             const data = await response.json()
@@ -34,6 +35,9 @@ module.exports = class {
             }
             entities["total"] = { // add alternate for total
                 alternates: ["india"],
+                value: ""
+            }
+            entities["source"] = {
                 value: ""
             }
             this.processor.updateEntities ("corona", entities)
@@ -54,20 +58,25 @@ module.exports = class {
         }) 
         if (states.length === 0) states = [ "total" ] // if no state is provided, default is "total"
 
-        return states.map (stateName => {
+        const data = states.map (stateName => {
             const data = this.dict[stateName]
             const recovery = ((data.recovered/data.confirmed)*100).toFixed(2)
-            const death = ((data.deaths/data.confirmed)*100).toFixed(2)
+            const death = ((data.deaths/data.recovered)*100).toFixed(2)
             const date = data.lastupdatedtime.slice(0, -3) // slice off seconds
             
             return `Covid-19 data (${stateName}) as of ${date}:\n` +
             `Active cases: ${ formatted(data.active) }\n` +
             `Confirmed cases: ${ formatted(data.confirmed) }\n` +
+            `Recovered cases: ${ formatted(data.recovered) }\n` +
             `Deaths: ${ formatted(data.deaths) }\n` +
             `Cases today: ${ formatted(data.deltaconfirmed) }\n` +
             `Recovery rate (active/confirmed): ${recovery}%\n` + 
-            `Death rate (deaths/confirmed): ${death}%\n`
+            `Death rate (deaths/recovered): ${death}%\n`
         })
-        .join ("\n\n")
+        data.push (
+            `Data from: ${API_URL}`
+        )
+        return data.join ("\n\n")
+        
     }
 }
