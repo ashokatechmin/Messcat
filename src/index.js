32b019b29741e2959b59db7e2f64097e3d1ee499
Adhiraj Singh <adhirajsingh1001@gmail.com>
2021-02-24 16:28:24 +0530

    Finish serverless

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 const LanguageProcessor = require('./intents')
 const { createSendMammyResponder } = require('@adiwajshing/whatsapp-info-bot')
 
-const hook = async(event) => {
-  const responder = createSendMammyResponder(
-    await LanguageProcessor,
+const responder = LanguageProcessor.then(processor => (
+  createSendMammyResponder(
+    processor,
     {
       delayMessage: 'Apologies for the delay in responding',
       minimumDelayTriggerS: 120,
       refreshToken: process.env.SENDMAMMY_REFRESH_TOKEN
     }
   )
-  return responder(event)
-}
-module.exports.hook = hook
+))
+
+module.exports.hook = async (event) => (await responder)(event)
