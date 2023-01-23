2f2cfc0737ff448baa06ffdc6b7e598939f212cd
SantriptaSharma <santripta@gmail.com>
2023-01-23 22:31:54 +0530

    fix (menu parser): Read from all available sheets in a workbook

--- src/utils/fetch-dining.ts
+++ src/utils/fetch-dining.ts
@@ -34,102 +34,104 @@ async function GetMenu()
     if (menu == undefined) throw new Error("Menu file not found");
     const parsed = await ParseDiningMenu(menu);
 
+    parsed.forEach(async weekMenu => {
+        const itemCounts = FindUniqueItems(weekMenu);
+        const itemNames = Array.from(itemCounts.keys());
+        console.log(itemCounts);
+
+        if ((await prisma.dailyMenu.findFirst({where: {date: weekMenu.start}})) != undefined)
+        {
+            console.log(`Menus already exist for week ${weekMenu.start.toDateString()} - ${weekMenu.end.toDateString()}`);
+            return;
+        }
+
+        const existingItems = await prisma.messItem.findMany({
+            where: {
+                name: {
+                    in: itemNames
+                }
+            },
+        });
+
+        const execDate = new Date();
+
+        const updatesToExistingItems = existingItems.map(item => prisma.messItem.update({
+            where: {id: item.id},
+            data: {
+                lastSeen: execDate,
+                count: itemCounts.get(item.name) ?? 0
+            }
+        }));
+
+        await prisma.$transaction(updatesToExistingItems);
+
+        const newItemNames = itemNames.filter(item => !existingItems.some(existing => existing.name == item));
+
+        await prisma.messItem.createMany({
+            data: newItemNames.map(item => ({name: item, count: itemCounts.get(item) ?? 0, lastSeen: execDate})),
+        });
+
+        for (let i = 0, date = weekMenu.start; i < 7; i++, date.setDate(date.getDate() + 1))
+        {
+            if ((await prisma.dailyMenu.findFirst({where: {date}})) != undefined)
+            {
+                throw new Error("Menu already exists for date " + date);
+            }
+            
+            const bfItems = (await prisma.messItem.findMany({
+                where: {
+                    name: {
+                        in: weekMenu.days[i].Breakfast.map(item => item.name)
+                    }
+                }
+            })).map(item => ({id: item.id}));
+
+            const lnItems = (await prisma.messItem.findMany({
+                where: {
+                    name: {
+                        in: weekMenu.days[i].Lunch.map(item => item.name)
+                    }
+                }
+            })).map(item => ({id: item.id}));
+
+            const snItems = (await prisma.messItem.findMany({
+                where: {
+                    name: {
+                        in: weekMenu.days[i].Snacks.map(item => item.name)
+                    }
+                }
+            })).map(item => ({id: item.id}));
+
+            const dnItems = (await prisma.messItem.findMany({
+                where: {
+                    name: {
+                        in: weekMenu.days[i].Dinner.map(item => item.name)
+                    }
+                }
+            })).map(item => ({id: item.id}));
+
+            await prisma.dailyMenu.create({
+                data: {
+                    date,
+                    breakfast: {
+                        connect: bfItems,
+                    },
+                    lunch: {
+                        connect: lnItems,
+                    },
+                    snacks: {
+                        connect: snItems,
+                    },
+                    dinner: {
+                        connect: dnItems,
+                    }
+                }
+            });
+        }
+    });
+
     if (process.argv.length >= 3)
     {
         await fs.rm(menu);
-    }
-
-    const itemCounts = FindUniqueItems(parsed);
-    const itemNames = Array.from(itemCounts.keys());
-    console.log(itemCounts);
-
-    if ((await prisma.dailyMenu.findFirst({where: {date: parsed.start}})) != undefined)
-    {
-        console.log(`Menus already exist for week ${parsed.start.toDateString()} - ${parsed.end.toDateString()}`);
-        return;
-    }
-
-    const existingItems = await prisma.messItem.findMany({
-        where: {
-            name: {
-                in: itemNames
-            }
-        },
-    });
-
-    const execDate = new Date();
-
-    const updatesToExistingItems = existingItems.map(item => prisma.messItem.update({
-        where: {id: item.id},
-        data: {
-            lastSeen: execDate,
-            count: itemCounts.get(item.name) ?? 0
-        }
-    }));
-
-    await prisma.$transaction(updatesToExistingItems);
-
-    const newItemNames = itemNames.filter(item => !existingItems.some(existing => existing.name == item));
-
-    await prisma.messItem.createMany({
-        data: newItemNames.map(item => ({name: item, count: itemCounts.get(item) ?? 0, lastSeen: execDate})),
-    });
-
-    for (let i = 0, date = parsed.start; i < 7; i++, date.setDate(date.getDate() + 1))
-    {
-        if ((await prisma.dailyMenu.findFirst({where: {date}})) != undefined)
-        {
-            throw new Error("Menu already exists for date " + date);
-        }
-        
-        const bfItems = (await prisma.messItem.findMany({
-            where: {
-                name: {
-                    in: parsed.days[i].Breakfast.map(item => item.name)
-                }
-            }
-        })).map(item => ({id: item.id}));
-
-        const lnItems = (await prisma.messItem.findMany({
-            where: {
-                name: {
-                    in: parsed.days[i].Lunch.map(item => item.name)
-                }
-            }
-        })).map(item => ({id: item.id}));
-
-        const snItems = (await prisma.messItem.findMany({
-            where: {
-                name: {
-                    in: parsed.days[i].Snacks.map(item => item.name)
-                }
-            }
-        })).map(item => ({id: item.id}));
-
-        const dnItems = (await prisma.messItem.findMany({
-            where: {
-                name: {
-                    in: parsed.days[i].Dinner.map(item => item.name)
-                }
-            }
-        })).map(item => ({id: item.id}));
-
-        await prisma.dailyMenu.create({
-            data: {
-                date,
-                breakfast: {
-                    connect: bfItems,
-                },
-                lunch: {
-                    connect: lnItems,
-                },
-                snacks: {
-                    connect: snItems,
-                },
-                dinner: {
-                    connect: dnItems,
-                }
-            }
-        })
     }
 })();
--- src/utils/parse-dining.ts
+++ src/utils/parse-dining.ts
@@ -14,63 +14,66 @@ type MessMenu = {
     days: {[k in Meal]: MessItem[]}[]
 }
 
-async function ParseXlsx(path: string): Promise<MessMenu>
+async function ParseXlsx(path: string): Promise<MessMenu[]>
 {
+    const res: MessMenu[] = [];
     const wb = new xl.Workbook();
     await wb.xlsx.readFile(path);
 
-    const menu = wb.worksheets[0];
+    wb.worksheets.forEach(menu => {
+        // TODO: Somehow check whether the sheet is a menu sheet, though this should be the case unless they decide to randomly change the format again
+
+        const meals: {[k in Meal]: xl.CellValue[][]} = {
+            Breakfast: menu.getRows(4, 8).map(row => row.values) as xl.CellValue[][],
+            Lunch: menu.getRows(13, 8).map(row => row.values) as xl.CellValue[][],
+            Snacks: menu.getRows(22, 5).map(row => row.values) as xl.CellValue[][],
+            Dinner: menu.getRows(28, 8).map(row => row.values) as xl.CellValue[][]
+        }
     
-    const meals: {[k in Meal]: xl.CellValue[][]} = {
-        Breakfast: menu.getRows(4, 8).map(row => row.values) as xl.CellValue[][],
-        Lunch: menu.getRows(13, 8).map(row => row.values) as xl.CellValue[][],
-        Snacks: menu.getRows(22, 5).map(row => row.values) as xl.CellValue[][],
-        Dinner: menu.getRows(28, 8).map(row => row.values) as xl.CellValue[][]
-    }
-
-    const title = menu.getCell(1, 1).value as string;
-    const startString = title.trim().split('(')[1].split('-')[0].trim().replace(/(rd)|(st)|(th)|(nd)/g, "") + ` ${ new Date().getFullYear()}`;
-    const startDate = new Date(Date.parse(startString));
-    startDate.setFullYear(new Date().getFullYear());
-    const endDate = new Date(startDate);
-    endDate.setDate(startDate.getDate() + 6)
-
-    const days: MessMenu["days"] =  Array.from(new Array(7), (_, i) => ({
-        Breakfast: [],
-        Lunch: [],
-        Snacks: [],
-        Dinner: []
-    }));
-
-    const exclude = ["-"];
-
-    Object.keys(meals).forEach(meal => {
-        const key = meal as Meal;
-
-        meals[key].forEach((row) => {
-            for (let i = 0; i < 7; i++)
-            {
-                const name = (row[i + 3] as string ?? "").trim();
-
-                if (name.length > 0 && !exclude.includes(name))
+        const title = menu.getCell(1, 1).value as string;
+        const startString = title.trim().split('(')[1].split('-')[0].trim().replace(/(rd)|(st)|(th)|(nd)/g, "") + ` ${ new Date().getFullYear()}`;
+        const startDate = new Date(Date.parse(startString));
+        startDate.setFullYear(new Date().getFullYear());
+        const endDate = new Date(startDate);
+        endDate.setDate(startDate.getDate() + 6)
+    
+        const days: MessMenu["days"] =  Array.from(new Array(7), (_, i) => ({
+            Breakfast: [],
+            Lunch: [],
+            Snacks: [],
+            Dinner: []
+        }));
+    
+        const exclude = ["-"];
+    
+        Object.keys(meals).forEach(meal => {
+            const key = meal as Meal;
+    
+            meals[key].forEach((row) => {
+                for (let i = 0; i < 7; i++)
                 {
-                    const split = name.split(",").map(s => s.trim()).filter(s => s.length > 0 && !exclude.includes(name)).map(s => s.split("(")[0].trim()).map(item => ({
-                        category: row[2] as string,
-                        name: item,
-                        meal: key
-                    }));
-
-                    days[i][key].push(...split);
+                    const name = (row[i + 3] as string ?? "").trim();
+    
+                    if (name.length > 0 && !exclude.includes(name))
+                    {
+                        const split = name.split(",").map(s => s.trim()).filter(s => s.length > 0 && !exclude.includes(name)).map(s => s.split("(")[0].trim()).map(item => ({
+                            category: row[2] as string,
+                            name: item,
+                            meal: key
+                        }));
+    
+                        days[i][key].push(...split);
+                    }
                 }
-            }
+            });
+        });
+
+        res.push({
+            start: startDate,
+            end: endDate,
+            days: days
         });
     });
-
-    const res: MessMenu = {
-        start: startDate,
-        end: endDate,
-        days: days
-    };
 
     return res;
 }
