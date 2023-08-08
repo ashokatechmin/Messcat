030f5fa924255c661716913a49de734f673815db
tantia16 <122145546+tantia16@users.noreply.github.com>
2023-08-08 13:02:24 +0530

    deleted  download-dining-menu.js
    dead file

--- src/utils/download-dining-menu.js
+++
@@ -1,8 +0,0 @@
-const { fetchLatestDiningMenu } = require('./fetch-dining-menus')
-const { writeDiningMenu } = require('./write-dining-menu')
-
-const download = async() => {
-	const { messMenuFilename, comboMenuFilename } = await fetchLatestDiningMenu()
-	await writeDiningMenu(messMenuFilename, comboMenuFilename)
-}
-download()
